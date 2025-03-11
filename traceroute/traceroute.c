efcae7abafcaea31987c8d791eb6223a19f74077
abde-r <elasri9999@gmail.com>
2025-03-11 15:51:59 +0000

    routetracer

--- traceroute/traceroute.c
+++ traceroute/traceroute.c
@@ -1,18 +1,10 @@
 #include "traceroute.h"
 
-s_data	data;
-
 /*
 	Initialization of data struct
 */
-void	struct_init(char	*address) {
-	data.address = strdup(address);
-
-	// data.statistics.packets_sent = 0;
-	// data.statistics.packets_received = 0;
-	// data.statistics.rtt_total = 0;
-	// data.statistics.rtt_min = 9999;
-	// data.statistics.rtt_max = 0;
+void	struct_init(s_data	*data, char	*address) {
+	data->address = strdup(address);
 }
 
 /*
@@ -49,84 +41,86 @@ unsigned short	checksum(void *packet, int len) {
 }
 
 /*
-    Send ICMP Echo Request
+    Send Packets
 */
-int	send_packets(struct sockaddr_in *addr, struct timeval *start, int ttl) {
-    char    packet[PACKET_SIZE];
-    struct  icmphdr *icmp = (struct icmphdr *)packet;
+int	send_packets(s_data	*data, struct timeval *start, int ttl, int probe) {
+	char sendPacket[PACKET_SIZE];
+	struct icmphdr icmpHeader;
 
-    memset(packet, 0, sizeof(packet));
-    icmp->type = ICMP_ECHO;
-    icmp->code = 0;
-    icmp->un.echo.id = htons(getpid());
-    icmp->un.echo.sequence = htons(ttl);
-    icmp->checksum = checksum(packet, PACKET_SIZE);
-    gettimeofday(start, NULL);
-    if (sendto(data.sockfd, packet, sizeof(packet), 0, (struct sockaddr *)addr, sizeof(* addr)) < 0) {
-        perror("sendto");
+	memset(sendPacket, 0, sizeof(sendPacket));
+	memset(&icmpHeader, 0, sizeof(icmpHeader));
+	icmpHeader.type = ICMP_ECHO;
+	icmpHeader.code = 0;
+	icmpHeader.un.echo.id = getpid();
+	icmpHeader.un.echo.sequence = ttl * PROBES_PER_HOP + probe;
+	icmpHeader.checksum = 0;
+	icmpHeader.checksum = checksum(&icmpHeader, sizeof(icmpHeader));
+	memcpy(sendPacket, &icmpHeader, sizeof(icmpHeader));
+      
+	gettimeofday(start, NULL);
+	if (sendto(data->sockfd, sendPacket, sizeof(icmpHeader), 0, (struct sockaddr *)&data->addr, sizeof(data->addr)) < 0) {
+		perror("sendto");
 		return 0;
-    }
-	return 1;
-}
-
-/*
-    Receive ICMP Echo Reply
-*/
-int	recv_packets(struct timeval *start, int ttl) {
-    char	packet[PACKET_SIZE];
-    struct	timeval end;
-
-	socklen_t addr_len = sizeof(data.addr);
-    gettimeofday(start, NULL);
-
-    // Receive data from the socket
-	if (recvfrom(data.sockfd, packet, sizeof(packet), 0, (struct sockaddr *)&data.addr, &addr_len) > 0) {
-    	gettimeofday(&end, NULL);
-		struct iphdr *ip_hdr = (struct iphdr *)packet;
-		struct icmphdr *icmp_hdr = (struct icmphdr *)(packet + (ip_hdr->ihl*4));
-
-		printf("%2d %s \n", ttl, inet_ntoa(data.addr.sin_addr));
-        double rtt = (end.tv_sec - start->tv_sec) * 1000.0 + (end.tv_usec - start->tv_usec) / 1000.0;
-		printf("%3f ms\n", rtt);
-		if (icmp_hdr->type == ICMP_ECHOREPLY)
-			return 0;
 	}
-	else
-		printf("%2d *\n", ttl);
 	return 1;
 }
 
 /*
 	Retrieving hostname from given IP address
 */
-void	resolveIP() {
+void	ip_resolver(s_data	*data) {
 	struct	sockaddr_in	sa;
 	struct	addrinfo	hints, *res;
 	struct	sockaddr_in	*addr;
 
-	if (inet_pton(AF_INET, data.address, &sa.sin_addr) != 0)
-		strcpy(data.ip, data.address);
+	if (inet_pton(AF_INET, data->address, &sa.sin_addr) != 0)
+		strcpy(data->ip, data->address);
 	else {
 		memset(&hints, 0, sizeof(hints));
 		hints.ai_family = AF_INET;
-		if (getaddrinfo(data.address, NULL, &hints, &res) != 0) {
-			fprintf(stderr, "Error: Could not resolve hostname %s\n", data.address);
-			freeaddrinfo(res);
+		if (getaddrinfo(data->address, NULL, &hints, &res) != 0) {
+			fprintf(stderr, "Error: Could not resolve hostname %s\n", data->address);
 			exit(EXIT_FAILURE);
 		}
 		addr = (struct sockaddr_in*)res->ai_addr;
-		strcpy(data.ip, inet_ntoa(addr->sin_addr));
+		strcpy(data->ip, inet_ntoa(addr->sin_addr));
 		freeaddrinfo(res);
 	}
+}
+
+/*
+	Print results for each hop
+*/
+void	hop_result(int ttl, double *rtts, struct sockaddr_in *recvAddr, char	*replyIP) {
+	printf("%2d  ", ttl);
+    if (replyIP[0] != '\0') {
+		char host[NI_MAXHOST];
+		if (getnameinfo((struct sockaddr *)recvAddr, sizeof(*recvAddr), host, sizeof(host), NULL, 0, NI_NAMEREQD) == 0) {
+			printf("%s (%s)  ", host, replyIP);
+		} else {
+			printf("%s (%s)  ", replyIP, replyIP);
+		}
+    } else {
+        printf("*  ");
+    }
+    for (int probe = 0; probe < PROBES_PER_HOP; probe++) {
+        if (rtts[probe] < 0)
+            printf("*  ");
+        else
+            printf("%.3f ms  ", rtts[probe]);
+    }
+    printf("\n");
 }
 
 void	ft_traceroute(char	*address) {
 	char	replyIP[INET_ADDRSTRLEN];
 	struct	sockaddr_in	recvAddr;
 	bool	destinationReached = false;
+	struct timeval start, end;
+	s_data	data;
 
-	struct_init(address);
-	resolveIP();
+	struct_init(&data, address);
+	ip_resolver(&data);
 	data.sockfd = create_socket();
 	printf("traceroute to %s (%s), %d hops max, %d byte packets\n", data.address, data.ip, MAX_HOPS, PACKET_SIZE);
 
@@ -150,29 +144,9 @@ void	ft_traceroute(char	*address) {
         
         // Send PROBES_PER_HOP probes for the current TTL
         for (int probe = 0; probe < PROBES_PER_HOP; probe++) {
-			
-			// Packets sending
-            char sendPacket[PACKET_SIZE];
-            memset(sendPacket, 0, sizeof(sendPacket));
-            
-            struct icmphdr icmpHeader;
-            memset(&icmpHeader, 0, sizeof(icmpHeader));
-            icmpHeader.type = ICMP_ECHO;
-            icmpHeader.code = 0;
-            icmpHeader.un.echo.id = getpid();
-            icmpHeader.un.echo.sequence = ttl * PROBES_PER_HOP + probe;
-            icmpHeader.checksum = 0;
-            icmpHeader.checksum = checksum(&icmpHeader, sizeof(icmpHeader));
-            memcpy(sendPacket, &icmpHeader, sizeof(icmpHeader));
-            
-            struct timeval start, end;
-            gettimeofday(&start, NULL);
-            if (sendto(data.sockfd, sendPacket, sizeof(icmpHeader), 0,
-                       (struct sockaddr *)&data.addr, sizeof(data.addr)) < 0) {
-                perror("sendto");
+            if (!send_packets(&data, &start, ttl, probe))
                 continue;
-            }
-            
+
 			// Packets receiving
             char recvPacket[512];
             socklen_t addrLen = sizeof(recvAddr);
@@ -185,31 +159,12 @@ void	ft_traceroute(char	*address) {
                 if (replyIP[0] == '\0') {
                     inet_ntop(AF_INET, &recvAddr.sin_addr, replyIP, sizeof(replyIP));
                 }
-                if (recvAddr.sin_addr.s_addr == data.addr.sin_addr.s_addr) {
+				if (recvAddr.sin_addr.s_addr == data.addr.sin_addr.s_addr || !data.addr.sin_addr.s_addr) {
                     destinationReached = 1;
                 }
             }
         }
-        
-        // Print results for this hop
-        printf("%2d  ", ttl);
-        if (replyIP[0] != '\0') {
-			char host[NI_MAXHOST];
-			if (getnameinfo((struct sockaddr *)&recvAddr, sizeof(recvAddr), host, sizeof(host), NULL, 0, NI_NAMEREQD) == 0) {
-				printf("%s (%s)  ", host, replyIP);
-			} else {
-				printf("%s (%s)  ", replyIP, replyIP);
-			}
-        } else {
-            printf("*  ");
-        }
-        for (int probe = 0; probe < PROBES_PER_HOP; probe++) {
-            if (rtts[probe] < 0)
-                printf("*  ");
-            else
-                printf("%.3f ms  ", rtts[probe]);
-        }
-        printf("\n");
+		hop_result(ttl, rtts, &recvAddr, replyIP);
     }
 	close(data.sockfd);
 }
--- traceroute/traceroute.h
+++ traceroute/traceroute.h
@@ -20,46 +20,6 @@
 #define MAX_HOPS 64
 #define PROBES_PER_HOP 3
 
-struct icmphdr {
-    uint8_t  type;     // ICMP type
-    uint8_t  code;     // ICMP code
-    uint16_t checksum; // Checksum
-    union {
-        struct {
-            uint16_t id;
-            uint16_t sequence;
-        } echo;
-        uint32_t gateway;
-        struct {
-            uint16_t unused;
-            uint16_t mtu;
-        } frag;
-    } un;
-};
-
-struct iphdr {
-    unsigned int ihl:4;      // Internet Header Length (in 32-bit words)
-    unsigned int version:4;  // IP version (4 for IPv4)
-    uint8_t tos;             // Type of Service
-    uint16_t tot_len;        // Total length (header + data)
-    uint16_t id;             // Identification
-    uint16_t frag_off;       // Fragment offset
-    uint8_t ttl;             // Time-to-Live
-    uint8_t protocol;        // Protocol (ICMP = 1, TCP = 6, UDP = 17)
-    uint16_t check;          // Header checksum
-    uint32_t saddr;          // Source IP address
-    uint32_t daddr;          // Destination IP address
-};
-
-// typedef	struct
-// {
-// 	int		packets_sent;
-// 	int		packets_received;
-// 	float	rtt_total;
-// 	float	rtt_min;
-// 	float	rtt_max;
-// }		s_statistics;
-
 typedef	struct
 {
 	int				sockfd;
@@ -67,7 +27,6 @@ typedef	struct
 	char			*address;
 	char			ip[INET_ADDRSTRLEN];
 	struct  		sockaddr_in addr;
-	// s_statistics	statistics;
 }		s_data;
 
 void	ft_traceroute(char	*address);
